6edd939aa3a17577502e8f65c623357bd63c7bb9
Benjamin Chanudet <dev@benjaminchanudet.com>
2025-09-21 22:31:35 +0000

    add configuration for using embeds

--- octoprint_octorant/events.py
+++ octoprint_octorant/events.py
@@ -1,3 +1,10 @@
+from enum import Enum
+
+class COLORS(Enum):
+    default = 5793266
+    green = 8319575
+    red = 15882071
+
 EVENTS = {
     # SYSTEM EVENTS
     "startup": {
@@ -5,12 +12,18 @@ EVENTS = {
         "media": "",
         "message": "⏰ I just woke up! What are we gonna print today?",
         "variables": [],
+        "embed_used": False,
+        "embed_fields": [],
+        "embed_color": COLORS.default
     },
     "shutdown": {
         "enabled": True,
         "media": "",
         "message": "💤 Going to bed now!",
         "variables": [],
+        "embed_used": False,
+        "embed_fields": [],
+        "embed_color": COLORS.default
     },
     # PRINTER EVENTS
     "printer_state_connecting": {
@@ -18,30 +31,45 @@ EVENTS = {
         "media": "",
         "message": "🔌 OctoPrint is connecting to your printer.",
         "variables": [],
+        "embed_used": False,
+        "embed_fields": [],
+        "embed_color": COLORS.default
     },
     "printer_state_operational": {
         "enabled": True,
         "media": "",
         "message": "✅ Your printer is operational.",
         "variables": [],
+        "embed_used": False,
+        "embed_fields": [],
+        "embed_color": COLORS.green
     },
     "printer_state_error": {
         "enabled": True,
         "media": "",
         "message": "⚠️ Your printer is in an erroneous state.",
         "variables": [],
+        "embed_used": False,
+        "embed_fields": [],
+        "embed_color": COLORS.red
     },
     "printer_state_unknown": {
         "enabled": True,
         "media": "",
         "message": "❔ Your printer is in an unknown state.",
         "variables": [],
+        "embed_used": False,
+        "embed_fields": [],
+        "embed_color": COLORS.red
     },
     "printer_state_offline": {
         "enabled": False,
         "media": "",
         "message": "❌ Your printer is offline.",
         "variables": [],
+        "embed_used": False,
+        "embed_fields": [],
+        "embed_color": COLORS.default
     },
     # PRINTS EVENTS
     "printing_started": {
@@ -49,18 +77,36 @@ EVENTS = {
         "media": "snapshot",
         "message": "🖨️ I've started printing **{name}**",
         "variables": ["name", "path", "origin", "size", "owner", "user", "size_formatted"],
+        "embed_used": False,
+        "embed_fields": [
+            ("Name", "name", True),
+            ("Size", "size_formatted", True)
+        ],
+        "embed_color": COLORS.default
     },
     "printing_paused": {
         "enabled": True,
         "media": "snapshot",
         "message": "⏸️ The printing was paused.",
         "variables": ["name", "path", "origin", "size", "owner", "user"],
+        "embed_used": False,
+        "embed_fields": [
+            ("Name", "name", True),
+            ("Size", "size_formatted", True)
+        ],
+        "embed_color": COLORS.default
     },
     "printing_resumed": {
         "enabled": True,
         "media": "snapshot",
         "message": "▶️ The printing was resumed.",
         "variables": ["name", "path", "origin", "size", "owner", "user"],
+        "embed_used": False,
+        "embed_fields": [
+            ("Name", "name", True),
+            ("Size", "size_formatted", True)
+        ],
+        "embed_color": COLORS.default
     },
     "printing_cancelled": {
         "enabled": True,
@@ -76,6 +122,12 @@ EVENTS = {
             "time",
             "time_formatted",
         ],
+        "embed_used": False,
+        "embed_fields": [
+            ("Name", "name", True),
+            ("Size", "size_formatted", True)
+        ],
+        "embed_color": COLORS.red
     },
     "printing_done": {
         "enabled": True,
@@ -91,6 +143,13 @@ EVENTS = {
             "time",
             "time_formatted",
         ],
+        "embed_used": False,
+        "embed_fields": [
+            ("Name", "name", True),
+            ("Size", "size_formatted", True),
+            ("Time spent", "time_formatted", True)
+        ],
+        "embed_color": COLORS.green
     },
     "printing_failed": {
         "enabled": True,
@@ -110,6 +169,15 @@ EVENTS = {
             "time_formatted", 
             "reason"
         ],
+        "embed_used": False,
+        "embed_fields": [
+            ("Name", "name", True),
+            ("Size", "size_formatted", True),
+            ("Time spent", "time_formatted", True),
+            ("Stopped at", "progress", False),
+            ("Reason", "reason", False)
+        ],
+        "embed_color": COLORS.red
     },
     # SD TRANSFERS EVENTS
     "transfer_started": {
@@ -117,18 +185,38 @@ EVENTS = {
         "media": "thumbnail",
         "message": "📼 Transfer started: {local} to {remote}",
         "variables": ["local", "remote"],
+        "embed_used": False,
+        "embed_fields": [
+            ("Local", "local", False),
+            ("Remote", "remote", False)
+        ],
+        "embed_color": COLORS.default
     },
     "transfer_done": {
         "enabled": False,
         "media": "",
         "message": "📼 Transfer done in {time_formatted}",
         "variables": ["local", "remote", "time", "time_formatted"],
+        "embed_used": False,
+        "embed_fields": [
+            ("Local", "local", False),
+            ("Remote", "remote", False),
+            ("Time spent", "time_formatted", True)
+        ],
+        "embed_color": COLORS.green
     },
     "transfer_failed": {
         "enabled": False,
         "media": "",
         "message": "📼 Transfer has failed! :(",
-        "variables": ["local", "remote", "time"],
+        "variables": ["local", "remote", "time", "time_formatted"],
+        "embed_used": False,
+        "embed_fields": [
+            ("Local", "local", False),
+            ("Remote", "remote", False),
+            ("Time spent", "time_formatted", True)
+        ],
+        "embed_color": COLORS.red
     },
     # PROGRESS EVENTS
     "printing_progress": {
@@ -140,6 +228,7 @@ EVENTS = {
             "path",
             "origin",
             "size",
+            "size_formatted",
             "owner",
             "user",
             "progress",
@@ -147,13 +236,27 @@ EVENTS = {
             "remaining",
             "spent_formatted",
             "remaining_formatted",
+        ],        
+        "embed_used": False,
+        "embed_fields": [
+            ("Name", "name", True),
+            ("Size", "size_formatted", True),
+            ("Progress", "progress", True),
+            ("Time spent", "time_formatted", True),
+            ("Remaining", "remaining_formatted", True)
         ],
+        "embed_color": COLORS.default
     },
     "transfer_progress": {
         "enabled": False,
         "media": "",
         "message": "📼 Transfer is at {progress}%",
-        "variables": ["progress"],
+        "variables": ["progress"],     
+        "embed_used": False,
+        "embed_fields": [
+            ("Progress", "progress", True)
+        ],
+        "embed_color": COLORS.default
     },
     # TIMELAPSES
     "timelapse_done": {
@@ -161,6 +264,12 @@ EVENTS = {
         "media": "timelapse",
         "message": "🎥 Timelapse has been created: {movie_basename}",
         "variables": ["gcode", "movie", "movie_basename", "movie_basename_uri", "movie_prefix"],
+        "embed_used": False,
+        "embed_fields": [
+            ("Gcode", "gcode", True),
+            ("Filename", "movie_basename", True),
+        ],
+        "embed_color": COLORS.green
     },
     "timelapse_failed": {
         "enabled": False,
@@ -176,11 +285,22 @@ EVENTS = {
             "error",
             "reason",
         ],
+        "embed_used": False,
+        "embed_fields": [
+            ("Gcode", "gcode", True),
+            ("Filename", "movie_basename", True),
+            ("Reason", "reason", False)
+        ],
+        "embed_color": COLORS.green
     },
     # Not a real message, but we will treat it as one
     "test": {
         "enabled": True,
         "media": "snapshot",
         "message": "Hello hello! If you see this message, it means that the settings are correct!",
+        "variables": [],
+        "embed_used": True,
+        "embed_fields": [],
+        "embed_color": COLORS.green
     },
 }
